4656b6fc05ecb20affebb4d60ca8b02f220c284f
mvelzel <meine.velzel@conneqt.com>
2021-05-31 17:01:41 +0200

    adds styles

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -9,5 +9,21 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography')
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "t-red": "#F23054",
+        "t-darkRed": "#BF263A",
+        "t-blue": "#056CF2",
+        "t-darkBlue": "#0455BF",
+        "t-darkerBlue": "#033E8C",
+        "t-green": "#22F2A6",
+        "t-black": "#222222"
+      }
+    },
+    fontFamily: {
+      'sans': 'Clash Grotesk, sans-serif'
+    }
+  }
 }
