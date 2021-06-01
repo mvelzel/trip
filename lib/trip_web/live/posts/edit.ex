d793e63e13b9ca6202b3042d4303f5cf7df36761
mvelzel <m.m.velzel@student.utwente.nl>
2021-06-02 00:20:03 +0200

    finishes posts

--- lib/trip_web/live/posts/edit.ex
+++ lib/trip_web/live/posts/edit.ex
@@ -1,10 +1,28 @@
 defmodule TripWeb.PostsLive.Edit do
   use TripWeb, :live_view
+  import Trip.ChangesetHelpers
+  import TripWeb.NestedFormHelpers
 
-  alias Trip.Posts.Post
-  alias Trip.Locations
+  alias Trip.Posts.{Post, PostLocation}
+  alias Trip.{Locations, Posts}
 
   @impl true
+  def mount(%{"post" => id}, session, socket) do
+    locations = Locations.list_locations()
+    post = Posts.get_post!(id)
+
+    changeset =
+      post
+      |> Post.changeset(%{})
+
+    {:ok,
+     socket
+     |> assign_defaults(session)
+     |> assign(locations: locations)
+     |> assign(post: post)
+     |> assign(changeset: changeset)}
+  end
+
   def mount(_params, session, socket) do
     locations = Locations.list_locations()
 
@@ -19,6 +37,19 @@ defmodule TripWeb.PostsLive.Edit do
      |> assign(changeset: changeset)}
   end
 
+  def handle_event(
+        "validate",
+        %{"post" => post_params},
+        %{assigns: %{live_action: :edit}} = socket
+      ) do
+    changeset =
+      socket.assigns.post
+      |> Post.changeset(post_params)
+      |> Map.put(:action, :update)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
   def handle_event("validate", %{"post" => post_params}, socket) do
     changeset =
       %Post{}
@@ -26,5 +57,53 @@ defmodule TripWeb.PostsLive.Edit do
       |> Map.put(:action, :insert)
 
     {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event(
+        "create",
+        %{"post" => post_params},
+        %{assigns: %{live_action: :edit}} = socket
+      ) do
+    {:ok, _} = Posts.update_post(socket.assigns.post, post_params)
+
+    {:noreply, push_redirect(socket, to: Routes.posts_index_path(socket, :index))}
+  end
+
+  def handle_event("create", %{"post" => post_params}, socket) do
+    {:ok, _} = Posts.create_post(post_params)
+
+    {:noreply, push_redirect(socket, to: Routes.posts_index_path(socket, :index))}
+  end
+
+  def handle_event("dropdown-selected", %{"value" => id}, socket) do
+    changeset =
+      socket.assigns.changeset
+      |> put_nested([:locations], fn chs ->
+        chs
+        |> Enum.concat([
+          PostLocation.changeset(
+            %PostLocation{
+              location_id: id
+            }
+            |> Trip.Repo.preload(:location),
+            %{}
+          )
+        ])
+      end)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("set-location", %{"location" => [lat, lon], "cursor" => cursor}, socket) do
+    changeset =
+      socket.assigns.changeset
+      |> change_nested(cursor, :latitude, lat)
+      |> change_nested(cursor, :longitude, lon)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("set-location", _params, socket) do
+    {:noreply, socket}
   end
 end
