287e0a4618ef6794cb0dba009d8da6c056324841
mvelzel <meine.velzel@conneqt.com>
2021-06-01 14:47:36 +0200

    lists locations on location index

--- lib/trip_web/live/locations/index.ex
+++ lib/trip_web/live/locations/index.ex
@@ -1,10 +1,15 @@
 defmodule TripWeb.LocationsLive.Index do
   use TripWeb, :live_view
 
+  alias Trip.Locations
+
   @impl true
   def mount(_params, session, socket) do
+    locations = Locations.list_locations()
+
     {:ok,
       socket
-      |> assign_defaults(session)}
+      |> assign_defaults(session)
+      |> assign(locations: locations)}
   end
 end
