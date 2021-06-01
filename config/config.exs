51bbf2c0cb057d6fee6686c0bd59b0188f5e2e6b
mvelzel <meine.velzel@conneqt.com>
2021-06-01 14:57:33 +0200

    adds nl translations

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,8 @@ config :kaffy,
    ecto_repo: Trip.Repo,
    router: TripWeb.Router
 
+config :trip, TripWeb.Gettext, default_locale: "nl"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
