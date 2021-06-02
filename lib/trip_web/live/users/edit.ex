1a0a88db885e597b45023682f6112f8d54aa76c1
mvelzel <m.m.velzel@student.utwente.nl>
2021-06-03 00:24:35 +0200

    implements postleader role in user edit page

--- lib/trip_web/live/users/edit.ex
+++ lib/trip_web/live/users/edit.ex
@@ -1,14 +1,20 @@
 defmodule TripWeb.UsersLive.Edit do
   use TripWeb, :live_view
+  import Trip.ChangesetHelpers
 
-  alias Trip.Accounts.User
-  alias Trip.{Locations, Accounts, Groups}
+  alias Trip.Accounts.{User, UserPost}
+  alias Trip.{Locations, Accounts, Groups, Posts}
 
   @impl true
   def mount(%{"user" => id}, session, socket) do
     locations = Locations.list_locations()
+    posts = Posts.list_posts()
     user = Accounts.get_user!(id)
-    groups = Groups.list_groups(user.group.location.id)
+    {selected_location, groups} = if !is_nil(user.group) do
+      {user.group.location.id, Groups.list_groups(user.group.location.id)}
+    else
+      {"", []}
+    end
 
     changeset =
       user
@@ -18,14 +24,16 @@ defmodule TripWeb.UsersLive.Edit do
      socket
      |> assign_defaults(session)
      |> assign(locations: locations)
-     |> assign(selected_location: user.group.location.id)
+     |> assign(selected_location: selected_location)
      |> assign(user: user)
      |> assign(groups: groups)
+     |> assign(posts: posts)
      |> assign(changeset: changeset)}
   end
 
   def mount(_params, session, socket) do
     locations = Locations.list_locations()
+    posts = Posts.list_posts()
 
     changeset =
       %User{}
@@ -35,6 +43,7 @@ defmodule TripWeb.UsersLive.Edit do
      socket
      |> assign_defaults(session)
      |> assign(locations: locations)
+     |> assign(posts: posts)
      |> assign(selected_location: "")
      |> assign(changeset: changeset)}
   end
@@ -58,6 +67,8 @@ defmodule TripWeb.UsersLive.Edit do
       |> User.registration_changeset(user_params)
       |> Map.put(:action, :insert)
 
+    IO.inspect(changeset)
+
     {:noreply, assign(socket, changeset: changeset)}
   end
 
@@ -80,10 +91,10 @@ defmodule TripWeb.UsersLive.Edit do
   def handle_event("location-selected", %{"value" => id}, socket) do
     groups = Groups.list_groups(id)
 
-    {:noreply, 
-      socket
-      |> assign(selected_location: id)
-      |> assign(groups: groups)}
+    {:noreply,
+     socket
+     |> assign(selected_location: id)
+     |> assign(groups: groups)}
   end
 
   def handle_event("group-selected", %{"value" => id}, socket) do
@@ -102,6 +113,43 @@ defmodule TripWeb.UsersLive.Edit do
       |> Ecto.Changeset.put_change(:role, role)
       |> User.validate_changeset()
 
+    {changeset, socket} =
+      case to_string(role) do
+        "player" ->
+          {changeset
+           |> Ecto.Changeset.put_assoc(:posts, [])
+           |> User.validate_changeset(), socket}
+
+        "postleader" ->
+          {changeset
+           |> Ecto.Changeset.put_change(:group_id, nil)
+           |> User.validate_changeset(), assign(socket, selected_location: "")}
+
+        _ ->
+          {changeset
+           |> Ecto.Changeset.put_change(:group_id, nil)
+           |> Ecto.Changeset.put_assoc(:posts, [])
+           |> User.validate_changeset(), assign(socket, selected_location: "")}
+      end
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("post-selected", %{"value" => id}, socket) do
+    changeset =
+      socket.assigns.changeset
+      |> put_nested([:posts], fn chs ->
+        chs
+        |> Enum.concat([
+          UserPost.changeset(
+            %UserPost{post_id: id}
+            |> Trip.Repo.preload(:post),
+            %{}
+          )
+        ])
+      end)
+      |> User.validate_changeset()
+    
     {:noreply, assign(socket, changeset: changeset)}
   end
 end
