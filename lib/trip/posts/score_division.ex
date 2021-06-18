030d2f8699c7931d64bb0bc030a9a0877517427e
mvelzel <m.m.velzel@student.utwente.nl>
2021-06-18 16:56:13 +0200

    imnplements removal of rank points

--- lib/trip/posts/score_division.ex
+++ lib/trip/posts/score_division.ex
@@ -1,18 +1,21 @@
 defmodule Trip.Posts.ScoreDivision do
   use Ecto.Schema
   import Ecto.Changeset
+  import Trip.ChangesetHelpers
   alias Trip.Posts.ScoreDivision
 
   @derive Jason.Encoder
   embedded_schema do
     field :points, :integer
+    field :delete, :boolean, virtual: true
   end
 
   @doc false
   def changeset(%ScoreDivision{} = score_division, attrs) do
     score_division
-    |> cast(attrs, [:points])
+    |> cast(attrs, [:points, :delete])
     |> validate_changeset()
+    |> maybe_mark_for_deletion()
   end
 
   def validate_changeset(changeset) do
